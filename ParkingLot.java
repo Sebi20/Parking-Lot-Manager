553bfb3645e1480c1b8b0552ee5a391cdded9e3b
Sebastian <sebastian.stephenson1@gmail.com>
2021-12-19 10:36:21 -0500

    Add function to return the cost

--- ParkingLot.java
+++ ParkingLot.java
@@ -3,17 +3,17 @@ package vehicle;
 import java.util.*;
 import java.io.*;
 import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 
 public class ParkingLot{
 	private Map<Car, LocalTime>lot;
 	final private int lotMax;// Maximum Parking spaces
-	final private double price; 
-	
-	
+	final private double price; // The price per hour of being in the lot
 			
 	
 	public ParkingLot(int max, double price) {
+		this.lot = new HashMap<Car, LocalTime>();
 		this.lotMax = max;
 		this.price = price;
 		
@@ -24,17 +24,26 @@ public class ParkingLot{
 		if(this.isFull()) {
 			System.out.println("Car lot is full");
 			return null;
-		}
+		}// End of if statement
+		
 		return lot.put(car, time);
 	
 	}// End of the checkIn method
 	
-	public LocalTime checkout(Car car, LocalTime time) {
+	public double checkout(Car car, LocalTime time) {
+		double cost;
 		if(lot.containsKey(car)) {
-			return lot.remove(car);
-		}
-		return null;
-	}
+			
+			/*As we remove the remove the car, the time is returned
+			we collect the time when removing the car and then use the
+			until function to find the difference in time in terms of hours passed.
+			This is then multiplied by the price to get the true cost of the car being in the lot. */
+			return cost = lot.remove(car).until(time, ChronoUnit.HOURS) * this.price;
+			
+		}// End of the if statement
+		return 0.0;// returns the 0.0 as default if the car isn't found in the lot
+	}// End of the checkout method
+	
 	
 	public boolean isFull() {
 		return this.lotMax == this.numOfCars();
@@ -54,6 +63,10 @@ public class ParkingLot{
 	
 	public boolean contains(Car car) {
 		return this.lot.containsKey(car);
+	}// End of the contains method
+	
+	public boolean contains(LocalTime time) {
+		return this.lot.containsValue(time);
 	}// End of the contains method
 	
 	
