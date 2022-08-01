c306d916b6947ed19e4a472bc2052c5a2b6c2838
Sebastian <sebastian.stephenson1@gmail.com>
2022-07-31 22:19:29 -0400

    Making this project into a GUI project, so I can learn

--- ParkingLotApp.java
+++ ParkingLotApp.java
@@ -3,6 +3,10 @@ It's written to emulate a parking lot check in and checkout system*/
 
 package vehicle;
 import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.JFrame;
 import java.io.*;
 import java.time.LocalTime;
 
@@ -22,17 +26,46 @@ public class ParkingLotApp{
 		price = 1.25;
 		
 		ParkingLot lot = new ParkingLot(MAX, price);
-		readFile(lot, file);
+		//readFile(lot, file);
 		
-		System.out.println(lot.numOfCars());
-		lot.displayCars();
-		
-		
+		//System.out.println(lot.numOfCars());
+		//lot.displayCars();
 		
 		
+		JFrame frame = new JFrame();// Creates an instance of a JFrame.
+		JLabel header = new JLabel();// Creates an instance of a JLabel. 
+		
+		
+		
+		frame.setSize(700, 700);
+		frame.setTitle("Parking Lot Manager");// Sets the title of the frame (Displayed at the top).
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// This will allow you change what you want the frame to do when the frame is closed
+			// This specifically allows the frame to be closed and not on in the background
+		//frame.setResizable(false);// This method allows the frame to be resizable if true and not resizable if false.
+		frame.getContentPane().setBackground(new Color(38, 40, 32));// Changes the color of the background
+		
+		
+		header.setText("Parking Lot Manager");
+		header.setVerticalAlignment(JLabel.TOP);
+		header.setHorizontalAlignment(JLabel.CENTER);
+		header.setForeground(Color.WHITE);
+		
+		
+		
+		
+		
+		
+		frame.add(header);
+		frame.setVisible(true);// Allows you to see the frame if true.
 		input.close();
 		input.close();
 	}// End of the main function
+	
+	
+	
+	
+	
+	
 	
 	public static void readFile(ParkingLot lot, Scanner file) {
 		String vin;
